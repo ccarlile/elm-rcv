ed9eb2c40a516978dea0bcca9470544471610f9a
Chris Carlile <christopher.carlile@gmail.com>
2019-10-10 12:31:00 -0500

    First commit

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with (import <nixpkgs> {});
+# let
+#   gems = bundlerEnv {
+#     name = "your-package";
+#     inherit ruby;
+#     gemdir = ./.;
+#   };
+# in stdenv.mkDerivation {
+stdenv.mkDerivation {
+  name = "elm-rcv";
+  buildInputs = [
+    elmPackages.elm
+    haskellPackages.elm-init
+    # haskellPackages.elm-reactor
+  ];
+}
